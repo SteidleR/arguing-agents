141c2f3106cfb0819ca6d899065e5a99fa3a8262
Makss Golubs <makss.golubs@ibm.com>
2021-11-15 17:38:24 +0100

    Add structure

--- agent.py
+++ agent.py
@@ -5,16 +5,25 @@ class Agent:
         self.cost_matrix = Agent.read_cost_matrix(file_name_costs)
 
     def evaluate_cost(self, contract):
-        pass
+        """
+        Calculates the costs for a series of jobs.
+        :param contract:
+        :return: costs as
+        """
+        costs = 0
+        for i in range(len(contract)-1):
+            costs += self.cost_matrix[i][i+1]
+        return costs
 
     @staticmethod
     def read_cost_matrix(file_name_costs):
+        """ Read cost matrix from text file
+
+        :param file_name_costs: file name/ path to cost matrix
+        :return: 2d cost matrix
+        """
         matrix = []
-        first_line = True
         with open(file_name_costs, "r") as f:
-            for line in f.readlines():
-                if not first_line:
-                    matrix.append([int(i) for i in line.replace(" \n", "").split(" ")])
-                else:
-                    first_line = False
+            for line in f.readlines()[1:]:
+                matrix.append([int(i) for i in line.replace(" \n", "").split(" ")])
         return matrix
--- main.py
+++ main.py
@@ -6,3 +6,8 @@ B = Agent("data/daten2B.txt")
 
 print(A.cost_matrix)
 print(B.cost_matrix)
+
+contract = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(A.evaluate_cost(contract))
+print(B.evaluate_cost(contract))
